5c3a2adf2a1fc1b5df7d988369744a0326b33f93
yulinshin <shin.yulin0516@gmail.com>
2022-01-07 11:41:26 +0800

    add user data model

---
+++ GitHubUserList/GitHubUserList/UserDataModel.swift
@@ -0,0 +1,51 @@
+//
+//  UserDataModel.swift
+//  GitHubUserList
+//
+//  Created by yulin on 2022/1/7.
+//
+
+import Foundation
+
+struct User {
+
+    let login: String
+    let id: Int
+    let nodeId: String
+    let avatarUrl: String
+    let gravatarId: String
+    let url: String
+    let htmlUrl: String
+    let followersUrl: String
+    let followingUrl: String
+    let gistsUrl: String
+    let starredUrl: String
+    let subscriptionsUrl: String
+    let organizationsUrl: String
+    let reposUrl: String
+    let eventsUrl: String
+    let receivedEventsUrl: String
+    let type: String
+    let siteAdmin: String
+
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeId
+        case avatarUrl = "avatar_url"
+        case gravatarId = "gravatar_id"
+        case url
+        case htmlUrl = "html_url"
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gistsUrl = "gists_url"
+        case starredUrl = "starred_url"
+        case subscriptionsUrl = "subscriptions_url"
+        case organizationsUrl = "organizations_url"
+        case reposUrl = "repos_url"
+        case eventsUrl = "events_url"
+        case receivedEventsUrl = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+    }
+}
