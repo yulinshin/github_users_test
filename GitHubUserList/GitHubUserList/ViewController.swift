0fa87594cb63c3479639f6a49f97d68d16b98567
yulinshin <shin.yulin0516@gmail.com>
2022-01-07 12:36:52 +0800

    setup BasicTableView

--- GitHubUserList/GitHubUserList/ViewController.swift
+++ GitHubUserList/GitHubUserList/ViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView = {
+        var table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.frame = self.view.bounds
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if let textlabel = cell.textLabel,
+           let imageView = cell.imageView {
+            textlabel.text = "login, isAdmin: True"
+            imageView.loadImage("picUrl")
+        }
+        return cell
     }
 
 }
-
